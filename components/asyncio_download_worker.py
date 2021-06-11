d0a8011cf3ca41925e225880263ee44f4f041b46
Sergey Lukashev <lukashev.s@ya.ru>
2021-06-12 01:41:59 +0300

    async download

---
+++ components/asyncio_download_worker.py
@@ -0,0 +1,235 @@
+import asyncio
+import itertools
+import logging
+import os
+from threading import Thread
+
+import aiofiles as aiofiles
+from PySide2.QtCore import QObject, Signal
+import aiohttp
+import m3u8
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class DownloadHandler:
+    log = logging.getLogger('DownloadHandler')
+    loop = asyncio.new_event_loop()
+    requests_queue = asyncio.Queue()
+
+    @classmethod
+    async def async_get_text(cls, url, session=None):
+        ses = session if session is not None else aiohttp.ClientSession()
+        async with ses:
+            for i in range(5):
+                if i > 0:
+                    await asyncio.sleep(i * 2)
+                    cls.log.info(f'retrying GET {url} after {2 * i} seconds')
+                try:
+                    async with ses.get(url) as response:
+                        return await response.text()
+                except aiohttp.ClientConnectorError as e:
+                    cls.log.error(str(e))
+
+    @classmethod
+    async def async_download(cls, url, filename, session=None):
+        ses = session if session is not None else aiohttp.ClientSession()
+        async with ses:
+            async with ses.request(method="GET", url=url) as response:
+                async with aiofiles.open(filename, "ba") as f:
+                    async for data in response.content.iter_chunked(256 * 1024):
+                        # cls.log.debug("writing data to %s", filename)
+                        await f.write(data)
+
+    @classmethod
+    def side_thread(cls, loop):
+        cls.log.info('started loop thread')
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+        cls.log.info('loop thread is exiting')
+
+    @classmethod
+    def init(cls):
+        cls.thread = Thread(target=cls.side_thread, args=(cls.loop,), daemon=False)
+        cls.thread.start()
+
+    @classmethod
+    def deinit(cls):
+        pass
+        cls.loop.call_soon_threadsafe(cls.loop.stop)
+        cls.thread.join()
+
+
+class DownloadFileWorker(QObject, DownloadHandler):
+    finished = Signal(float, int)
+    errored = Signal(str)
+
+    def create_signals(self):
+        pass
+    def __init__(self, url, path, length, index):
+        super(DownloadFileWorker, self).__init__()
+
+        self.create_signals()
+
+        self.url = url
+        self.path = path
+        self.length = length
+        self.index = index
+
+    async def run(self):
+        await self.async_download(self.url, self.path)
+
+    def start(self):
+        task = self.loop.create_task(self.run())
+        def on_done(f: asyncio.Future):
+            try:
+                if not f.cancelled():
+                    f.result()
+            except Exception as e:
+                error_msg = f'downloading of {self.url} to {self.path} failed: {str(e)}'
+                self.log.error(error_msg)
+                self.errored.emit(error_msg)
+            else:
+                self.log.info('finished downloading %s to %s', self.url, self.path)
+                self.finished.emit(self.length, self.index)
+            finally:
+                self.deleteLater()
+
+        task.add_done_callback(on_done)
+
+
+class DownloadVideoWorker(QObject, DownloadHandler):
+    started = Signal()
+    downloaded_chunk = Signal(float)
+    stopped = Signal()
+    errored = Signal(str)
+
+    def create_signals(self):
+        pass
+
+    def __init__(self, uik_obj, index, stream_url):
+        super(DownloadVideoWorker, self).__init__()
+
+        self.create_signals()
+
+        self.uik_obj = uik_obj
+        self.index = index
+        self.directory = self.last_file = ''
+        self.hls_url = stream_url
+        self.part_num = self.media_sequence = 0
+        self.meta = self.hls = None
+        self.stop = self.error = False
+        self.tasks = {}
+
+    def _get_available_dir(self):
+        for i in itertools.count():
+            path = os.path.join('output', f'{self.uik_obj["uik"]} ({i})')
+            if not os.path.exists(path):
+                return path
+
+    def _dl_chunk(self, url, path, length, index):
+        url = url.replace('mono.m3u8', '')
+
+        dl_worker = DownloadFileWorker(url, path, length, index)
+        dl_worker.finished.connect(self.on_downloaded_chunk)
+        dl_worker.errored.connect(self.on_error)
+        self.tasks[index] = dl_worker
+        dl_worker.start()
+
+    async def load_hls(self):
+        # print (self.hls_url)
+        # ff = open('d:\\tmp\\#cam\\test.txt', 'wb')
+        try:
+        #     with tempfile.TemporaryDirectory() as dir:
+        #         file_path = os.path.join(dir, 'playlist.m3u8')
+        #         with open(file_path, 'wb') as f:
+        #             # f.write(requests.get(self.hls_url, verify=False).content)
+        #             a = requests.get(self.hls_url, verify=False).content
+        #             # f.write(a.replace('2020','https://flu01.pride-net.ru/cam_60let59/tracks-v1/2020'))
+        #             # print (a)
+        #             f.write(a)
+        #             # f.write(requests.get(self.hls_url, verify=False).content)
+        #             # ff.write(requests.get(self.hls_url, verify=False).content)
+        #             # ff.write(a)
+        #         # return m3u8.load(file_path)
+        #
+        #         m3u8_obj = m3u8.load('https://flu01.pride-net.ru/cam_60let59/tracks-v1/mono.m3u8')
+        #
+        #         print (m3u8_obj.segments)
+        #         print (m3u8_obj.target_duration)
+        #
+        #         # return m3u8.load('https://flu01.pride-net.ru/cam_60let59/tracks-v1/mono.m3u8')
+        #         print (requests.get(self.hls_url, verify=False).text)
+                hls_playlist = await self.async_get_text(self.hls_url)
+
+                return m3u8.loads(hls_playlist)
+
+        except Exception as e:
+            self.log.error(str(e))
+            return None
+
+    def on_error(self, msg):
+        self.error = True
+        self.errored.emit(msg)
+
+    def on_downloaded_chunk(self, length, index):
+        self.downloaded_chunk.emit(length)
+        del self.tasks[index]
+
+    async def run(self):
+        self.directory = self._get_available_dir()
+
+        try:
+            os.makedirs(self.directory)
+        except OSError:
+            self.errored.emit(f'failed to create directory {self.directory}')
+            return
+
+        self.started.emit()
+
+        hls = await self.load_hls()
+        prev_hls = None
+        self.media_sequence = hls.media_sequence
+        while True:
+            if self.stop:
+                self.stopped.emit()
+                break
+            if self.error:
+                break
+            try:
+                idx = hls.files.index(self.last_file) + 1
+            except ValueError:
+                if self.media_sequence != hls.media_sequence:
+                    idx = 0
+                else:
+                    idx = -1
+            self.last_file = hls.files[-1]
+            for i, file in enumerate(hls.files[idx:]):
+                file_name = os.path.join(self.directory, f'part.{hls.media_sequence:08d}.{self.part_num:06d}.ts')
+                self._dl_chunk(self.hls_url + file, file_name, hls.segments[idx+i].duration, self.part_num)
+                self.part_num += 1
+            await asyncio.sleep(hls.target_duration - 2)
+            prev_hls = hls
+            while True:
+                hls = await self.load_hls()
+                if hls and hls.files[-1] != prev_hls.files[-1]:
+                    break
+                await asyncio.sleep(0.2)
+
+    def start(self):
+        def on_done(f: asyncio.Future):
+            try:
+                if not f.cancelled():
+                    f.result()
+            except Exception as e:
+                error_msg = f'processing of stream {self.hls_url} failed: {str(e)}'
+                self.log.error(error_msg)
+                self.on_error(error_msg)
+            else:
+                self.log.info(f'finished processing of stream {self.hls_url}')
+            finally:
+                self.deleteLater()
+
+        f = asyncio.run_coroutine_threadsafe(self.run(), self.loop)
+        f.add_done_callback(on_done)
